4f5c946e68f8c48a1dc8804ce6c24f0f5cc6d6a2
fabxy <fwaschkowski@student.unimelb.edu.au>
2024-02-27 20:55:01 +1100

    translation v1

--- test_transformer.py
+++ test_transformer.py
@@ -1,69 +1,40 @@
 import torch
-import torch.nn as nn
-from torch import Tensor
 
 from torchtext.data.utils import get_tokenizer
 from torchtext.vocab import build_vocab_from_iterator
 from torchtext.datasets import multi30k, Multi30k
-from typing import Iterable, List
-
-from torch.utils.data import DataLoader
-from torch.nn.utils.rnn import pad_sequence
 
 from transformer import Transformer
-from tqdm import tqdm
-import math
 
-DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-# Get data
-# We need to modify the URLs for the dataset since the links to the original dataset are broken
-# Refer to https://github.com/pytorch/text/issues/1756#issuecomment-1163664163 for more info
+# Set up data pipeline
 multi30k.URL["train"] = "https://raw.githubusercontent.com/neychev/small_DL_repo/master/datasets/Multi30k/training.tar.gz"
 multi30k.URL["valid"] = "https://raw.githubusercontent.com/neychev/small_DL_repo/master/datasets/Multi30k/validation.tar.gz"
+multi30k.URL["test"] = "https://raw.githubusercontent.com/neychev/small_DL_repo/master/datasets/Multi30k/mmt_task1_test2016.tar.gz"
+multi30k.MD5["test"] = "876a95a689a2a20b243666951149fd42d9bfd57cbbf8cd2c79d3465451564dd2"
 
 SRC_LANGUAGE = 'de'
 TGT_LANGUAGE = 'en'
 
-# Place-holders
 token_transform = {}
 vocab_transform = {}
 
 token_transform[SRC_LANGUAGE] = get_tokenizer('spacy', language='de_core_news_sm')
 token_transform[TGT_LANGUAGE] = get_tokenizer('spacy', language='en_core_web_sm')
 
-# Define special symbols and indices
 UNK_IDX, PAD_IDX, BOS_IDX, EOS_IDX = 0, 1, 2, 3
-
-# Make sure the tokens are in order of their indices to properly insert them in vocab
 special_symbols = ['<unk>', '<pad>', '<bos>', '<eos>']
 
-# helper function to yield list of tokens
-def yield_tokens(data_iter: Iterable, language: str) -> List[str]:
+def yield_tokens(data_iter, language):
     language_index = {SRC_LANGUAGE: 0, TGT_LANGUAGE: 1}
 
     for data_sample in data_iter:
         yield token_transform[language](data_sample[language_index[language]])
 
 for ln in [SRC_LANGUAGE, TGT_LANGUAGE]:
-    # Training data Iterator
     train_iter = Multi30k(split='train', language_pair=(SRC_LANGUAGE, TGT_LANGUAGE))
-    # Create torchtext's Vocab object
-    vocab_transform[ln] = build_vocab_from_iterator(yield_tokens(train_iter, ln),
-                                                    min_freq=1,
-                                                    specials=special_symbols,
-                                                    special_first=True)
+    vocab_transform[ln] = build_vocab_from_iterator(yield_tokens(train_iter, ln), min_freq=1, specials=special_symbols, special_first=True)
+    vocab_transform[ln].set_default_index(UNK_IDX)
     
-# Set ``UNK_IDX`` as the default index. This index is returned when the token is not found.
-# If not set, it throws ``RuntimeError`` when the queried token is not found in the Vocabulary.
-for ln in [SRC_LANGUAGE, TGT_LANGUAGE]:
-  vocab_transform[ln].set_default_index(UNK_IDX)
-
-# Create data loader
-train_iter = Multi30k(split='train', language_pair=(SRC_LANGUAGE, TGT_LANGUAGE))
-BATCH_SIZE = 128
-
-# helper function to club together sequential operations
 def sequential_transforms(*transforms):
     def func(txt_input):
         for transform in transforms:
@@ -71,32 +42,13 @@ def sequential_transforms(*transforms):
         return txt_input
     return func
 
-# function to add BOS/EOS and create tensor for input sequence indices
-def tensor_transform(token_ids: List[int]):
-    return torch.cat((torch.tensor([BOS_IDX]),
-                      torch.tensor(token_ids),
-                      torch.tensor([EOS_IDX])))
+def tensor_transform(token_ids):
+    return torch.cat((torch.tensor([BOS_IDX]), torch.tensor(token_ids), torch.tensor([EOS_IDX])))
 
-# ``src`` and ``tgt`` language text transforms to convert raw strings into tensors indices
 text_transform = {}
 for ln in [SRC_LANGUAGE, TGT_LANGUAGE]:
-    text_transform[ln] = sequential_transforms(token_transform[ln], #Tokenization
-                                               vocab_transform[ln], #Numericalization
-                                               tensor_transform) # Add BOS/EOS and create tensor
-    
-# function to collate data samples into batch tensors
-def collate_fn(batch):
-    src_batch, tgt_batch = [], []
-    for src_sample, tgt_sample in batch:
-        src_batch.append(text_transform[SRC_LANGUAGE](src_sample.rstrip("\n")))
-        tgt_batch.append(text_transform[TGT_LANGUAGE](tgt_sample.rstrip("\n")))
+    text_transform[ln] = sequential_transforms(token_transform[ln], vocab_transform[ln], tensor_transform)
 
-    src_batch = pad_sequence(src_batch, padding_value=PAD_IDX)
-    tgt_batch = pad_sequence(tgt_batch, padding_value=PAD_IDX)
-    return src_batch, tgt_batch
-
-train_dataloader = DataLoader(train_iter, batch_size=BATCH_SIZE, collate_fn=collate_fn)
-num_batches = math.ceil(multi30k.NUM_LINES['train'] / BATCH_SIZE)
 
 # Set seed
 torch.manual_seed(0)
@@ -118,60 +70,32 @@ hyperparams = {
 
 model = Transformer(**hyperparams)
 
-# Set up tutorial model for comparison
-from transformer_tutorial import Seq2SeqTransformer, create_mask
+# Load weights
+load_path = "first_model.pt"
+load_state = torch.load(load_path)
 
-transformer = Seq2SeqTransformer(hyperparams['nenc'], hyperparams['ndec'], hyperparams['dm'], hyperparams['h'], hyperparams['dvocin'], hyperparams['dvocout'], hyperparams['dff'], hyperparams['drop'])
-transformer = transformer.to(DEVICE)
-
-for p in transformer.parameters():
-    if p.dim() > 1:
-        nn.init.xavier_uniform_(p)
-
-# Copy weights
-from transformer_tools import update_state_dict
 model_state = model.state_dict()
-transformer_state = transformer.state_dict()
+model_state.update(load_state)
 
-model_state = update_state_dict(transformer_state, model_state)
 model.load_state_dict(model_state)
 
-# Define training
-train_params = {
-    'beta1': 0.9,
-    'beta2': 0.98,
-    'eps': 1e-9,
-    'lr': 1e-4,
-    'init_steps': 4000,
-    'steps': int(1e5),
-    'ignore_idx': PAD_IDX,
-    'eps_ls': 0.0,
-    'epochs': 18
-}
+# Translate test sentences
+val_iter = Multi30k(split='valid', language_pair=(SRC_LANGUAGE, TGT_LANGUAGE))
 
-optimizer = torch.optim.Adam(model.parameters(), lr=train_params['lr'], betas=(train_params['beta1'], train_params['beta2']), eps=train_params['eps'])
+max_len = 50
 
-# lr_lambda = lambda step: hyperparams['dm']**(-0.5) * min((step+1)**(-0.5), (step+1)**(-0.5) * train_params['init_steps']**(-1.5))
-# scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lr_lambda)
+model.eval()
+for src_seq, tgt_seq in val_iter:
 
-loss_fun = torch.nn.CrossEntropyLoss(ignore_index=train_params['ignore_idx'], label_smoothing=train_params['eps_ls'])
+    print(f"Translate: {src_seq}")
 
-for epoch in range(train_params['epochs']):
+    src = text_transform[SRC_LANGUAGE](src_seq).unsqueeze(-1)
+    tgt = torch.ones((1,1), dtype=src.dtype) * hyperparams['stok']
 
-    model.train()
-    for src, tgt in (p := tqdm(train_dataloader, desc=f"Epoch {epoch}", total=num_batches)):
+    while (len(tgt) < max_len) and (tgt[-1].item() != hyperparams['etok']):
+        logits = model(src, tgt)
+        probs = torch.softmax(logits, dim=-1)
+        t = torch.argmax(probs, dim=-1)
+        tgt = torch.cat((tgt, t[-1:,:]), dim=0)
 
-        optimizer.zero_grad()
-
-        probs = model(src, tgt[:-1])
-        
-        src_mask, tgt_mask, src_padding_mask, tgt_padding_mask = create_mask(src, tgt[:-1], pad_idx=PAD_IDX, device=DEVICE)
-        probs_transformer = transformer(src, tgt[:-1], src_mask, tgt_mask, src_padding_mask, tgt_padding_mask, src_padding_mask)
-
-        loss = loss_fun(torch.permute(probs, (1, 2, 0)), tgt[1:].T)
-        loss.backward()
-
-        optimizer.step()
-        # scheduler.step()
-
-        p.set_postfix(loss=loss.item())
+    print(f"Translation: {' '.join(vocab_transform['en'].lookup_tokens(tgt[1:,:].reshape(-1).tolist())).replace('<bos>', '').replace('<eos>', '')}\n")
